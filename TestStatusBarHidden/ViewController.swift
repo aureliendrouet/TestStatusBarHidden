f75fd9141c27048078952be422b3932ba38f1ed4
Yosuke Yamamoto <hellfire0918@gmail.com>
2016-12-20 13:49:39 +0900

    initial

--- TestStatusBarHidden/ViewController.swift
+++ TestStatusBarHidden/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var statusBarHiddenFlag: Bool = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btAction(_ sender: Any) {
+        self.statusBarHiddenFlag = !self.statusBarHiddenFlag
+        super.setNeedsStatusBarAppearanceUpdate()
+    }
+
+    override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
+        return UIStatusBarAnimation.slide
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return self.statusBarHiddenFlag
+    }
 
 }
 
